a88b7cea4a14ec0d03ccb3d5a8a20941535aae77
Xi Qi <realxqi@gmail.com>
2016-03-10 22:47:32 +0800

    Revert "customize AuditingHandler"
    This reverts commit abf2c76b86e62b4b4ac67197fec4e0b8ef381b21.

--- src/main/java/org/springframework/data/jpa/repository/config/PublicJpaAuditingRegistrar.java
+++
@@ -1,5 +0,0 @@
-package org.springframework.data.jpa.repository.config;
-
-public class PublicJpaAuditingRegistrar extends JpaAuditingRegistrar {
-    public static final String JPA_MAPPING_CONTEXT_BEAN_NAME = BeanDefinitionNames.JPA_MAPPING_CONTEXT_BEAN_NAME;
-}
